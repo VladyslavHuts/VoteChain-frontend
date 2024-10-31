9ed633d46d769fc1594eeae8fa9c7eae2a67512e
Vladyslav Huts < v.huts.work@gmail.com>
2024-10-31 18:59:59 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/main.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>fsd</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
