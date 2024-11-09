fe8485dc4e7f8f18cffba5b71659cd4701cedef8
RomanHuriev <roman.huriev.p@gmail.com>
2024-11-09 20:19:49 +0100

    Опис змін, ÐÐHeader Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/header.css';
+import logo from '../assets/images/header__logo.svg';
 
-// Якщо компонент не має пропсів, вказуємо пустий об'єкт {}
-interface HeaderProps {}
 
-class Header extends Component<HeaderProps> {
-    componentDidMount() {
-        const number: number = 42; // визначаємо число з типом "number"
-        console.log("Число:", number); // виводимо в консоль
-    }
-
+class Header extends Component {
     render() {
         return (
             <div className="header">
-                <p>fdsfsd</p>
+                <div className="container">
+                    <div className="header__wrap">
+                        <div className="header__container">
+                            <img className="header__logo" src={logo} alt=""/>
+                            <nav className="header__nav">
+                                <ul className="header__list">
+                                    <li className="header__item"><a href=""> VoteChain</a></li>
+                                    <li className="header__item"><a href="">About the platform</a></li>
+                                    <li className="header__item"><a href="">How it works</a></li>
+                                    <li className="header__item"><a href="">Active votes</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <button className="header__btn">Log in </button>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+        <React.StrictMode>
+            <div className="root">
+                <Header/>
+                <App/>
+                <Footer/>
+            </div>
+        </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
