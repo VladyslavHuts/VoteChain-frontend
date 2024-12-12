0d92f51579a1b98affeb50d395d76c3ef36795a9
RomanHuriev <roman.huriev.p@gmail.com>
2024-12-12 16:24:58 +0100

    A script for the header has been made and the appearance of the main page has been improved.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,87 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/header.css';
 import logo from '../assets/images/header__logo.svg';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
+interface HeaderState {
+    isLoggedIn: boolean;
+}
 
-class Header extends Component {
+class Header extends Component<{}, HeaderState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+        };
+    }
+
+    toggleLogin = () => {
+        this.setState((prevState) => ({ isLoggedIn: !prevState.isLoggedIn }));
+    };
+
     render() {
+        const { isLoggedIn } = this.state;
+
         return (
             <div className="header">
                 <div className="container">
-                    <div className="header__container ">
+                    <div className="header__container">
                         <div className="header__wrap">
-                            <img className="header__logo" src={logo} alt="VoteChain"/>
+                            <Link  to="/">
+                                <img className="header__logo" src={logo} alt="VoteChain" />
+                            </Link>
                             <nav className="header__nav">
                                 <ul className="header__list">
                                     <li className="header__item">
-                                        <Link className="header__link" to="/">VoteChain</Link>
+                                        <NavLink
+                                            className={({ isActive }) =>
+                                                isActive ? "header__link active" : "header__link"
+                                            }
+                                            to="/"
+                                        >
+                                            VoteChain
+                                        </NavLink>
                                     </li>
-                                    <li className="header__item"><Link to="">About the platform</Link></li>
-                                    <li className="header__item"><Link to="">How it works</Link></li>
                                     <li className="header__item">
-                                        <Link className="header__link" to="/active-votes">Active votes</Link>
+                                        <NavLink
+                                            className={({ isActive }) =>
+                                                isActive ? "header__link active" : "header__link"
+                                            }
+                                            to="/about"
+                                        >
+                                            About the platform
+                                        </NavLink>
+                                    </li>
+                                    <li className="header__item">
+                                        <NavLink
+                                            className={({ isActive }) =>
+                                                isActive ? "header__link active" : "header__link"
+                                            }
+                                            to="/how-it-works"
+                                        >
+                                            How it works
+                                        </NavLink>
+                                    </li>
+                                    <li className="header__item">
+                                        <NavLink
+                                            className={({ isActive }) =>
+                                                isActive ? "header__link active" : "header__link"
+                                            }
+                                            to="/active-votes"
+                                        >
+                                            Active votes
+                                        </NavLink>
                                     </li>
                                 </ul>
                             </nav>
                         </div>
-                        <button className="header__btn">Log in</button>
+                        <button
+                            className="header__btn"
+                            onClick={this.toggleLogin}
+                        >
+                            {isLoggedIn ? 'Account' : 'Log in'}
+                        </button>
                     </div>
                 </div>
             </div>
